0431aa962372564086497ce28bbc8c34e804951b
Pedro Dantas <pedrood1232@gmail.com>
2024-02-14 15:44:07 -0300

    Melhorias no layout

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tkinter as tk
+import tkinter.font as tkfont
 import tkinter.messagebox as msg
 from pathlib import Path
 
@@ -9,11 +10,8 @@ from tarefa import Tarefa
 
 
 def assets(path: str, frame: str) -> Path:
-    # path = os.getcwd() / Path("assets") / Path(frame) / Path(path)
-    # return path
     path = Path("assets") / Path(frame) / Path(path)
     try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
@@ -30,11 +28,12 @@ window = tk.Tk()
 
 # Configurando a janela
 window.title("Minha Agenda - v1.0")
-width, height = window.winfo_screenwidth(), window.winfo_screenheight()
-width -= 100
-height -= 80
+# width, height = window.winfo_screenwidth(), window.winfo_screenheight()
+# width -= 100
+# height -= 80
+width = 1060
+height = 640
 window.geometry("%dx%d+0+0" % (width, height))
-# window.geometry("1440x800")
 window.configure(bg="#FFFFFF")
 window.resizable(False, False)
 
@@ -63,7 +62,7 @@ def startScreen():
     frame.place_forget()
     frame_new.place_forget()
     frame_tasks.place_forget()
-    frame_inicio.place(x=0, y=0, width=1440, height=950)
+    frame_inicio.place(x=0, y=0, width=width, height=height)
 
 
 def break_text_by_length(text, max_length):
@@ -87,8 +86,8 @@ def break_text_by_length(text, max_length):
 canvas_tasks = tk.Canvas(
     frame_tasks,
     bg="#FFFFFF",
-    height=950,
-    width=1052,
+    height=height,
+    width=0.73 * width,
     bd=1.75,
     highlightthickness=0,
     relief="ridge",
@@ -97,15 +96,15 @@ canvas_tasks.place(x=14, y=0)
 
 
 def drawTasks(agenda):
-    wid = 1052
-    hei = 950
+    wid = 0.73 * width
+    hei = height
 
-    taskWid = 290
-    taskHei = 280
+    taskWid = 0.2 * width
+    taskHei = 0.3 * height
 
-    offset_x = 50
-    offset_y = 50
-    space_between_tasks = 45
+    offset_x = 20
+    offset_y = 20
+    space_between_tasks = 30
     max_tasks_per_row = 1
 
     tasks_lists = [
@@ -113,7 +112,7 @@ def drawTasks(agenda):
         agenda.tarefas_executando,
         agenda.tarefas_concluidas,
     ]
-    canvas_tasks.create_rectangle(0, 0, 1060, 950, fill="#FFFFFF")
+    canvas_tasks.create_rectangle(0, 0, 0.73 * width, height, fill="#FFFFFF")
 
     for j, task_list in enumerate(tasks_lists):
         # Calcular a posição de cada tarefa na tela
@@ -173,8 +172,10 @@ def mainScreen():
     info.screen = 1
     frame_inicio.place_forget()
     frame_new.place_forget()
-    frame.place(x=0, y=0, width=1440, height=950)
-    frame_tasks.place(x=348, y=100, width=1092, height=950)
+    frame.place(x=0, y=0, width=width, height=height)
+    frame_tasks.place(
+        x=0.24 * width, y=0.105 * height, width=0.75 * width, height=height
+    )
 
 
 def novaTarefaScreen():
@@ -182,7 +183,7 @@ def novaTarefaScreen():
     frame_inicio.place_forget()
     frame.place_forget()
     frame_tasks.place_forget()
-    frame_new.place(x=0, y=0, width=1440, height=950)
+    frame_new.place(x=0, y=0, width=width, height=height)
 
 
 def click():
@@ -235,15 +236,15 @@ def editTask(agenda):
 
 
 def selectTask(event, agenda):
-    wid = 1052
-    hei = 950
+    wid = 0.73 * width
+    hei = height
 
-    taskWid = 290
-    taskHei = 280
+    taskWid = 0.2 * width
+    taskHei = 0.3 * height
 
-    offset_x = 50
-    offset_y = 50
-    space_between_tasks = 45
+    offset_x = 20
+    offset_y = 20
+    space_between_tasks = 30
     max_tasks_per_row = 1
 
     tasks_lists = [
@@ -282,22 +283,22 @@ startScreen()
 canvas = tk.Canvas(
     frame,
     bg="#FFFFFF",
-    height=950,
-    width=1440,
+    height=height,
+    width=width,
     bd=0,
     highlightthickness=0,
     relief="ridge",
 )
 
 canvas.place(x=0, y=0)
-canvas.create_rectangle(0, 0, 348, 950, fill="#05ACF5", outline="")
+canvas.create_rectangle(0, 0, 0.24 * width, height, fill="#05ACF5", outline="")
 
 
 start_logo = tk.PhotoImage(file=assets("image_1.png", "frame1"))
-start_logo_img = canvas.create_image(1369.0, 50, image=start_logo)
+start_logo_img = canvas.create_image(0.95 * width, 0.05 * height, image=start_logo)
 
 start_logo_2 = tk.PhotoImage(file=assets("image_2.png", "frame1"))
-start_logo_img_2 = canvas.create_image(914.0, 0.0, image=start_logo_2)
+start_logo_img_2 = canvas.create_image(0.634 * width, 0.0, image=start_logo_2)
 
 
 # Criando os botões da tela principal
@@ -311,7 +312,12 @@ for i in range(0, 6):
 
     button_1 = tk.Button(frame, image=photo)
     button_1.image = photo
-    button_1.place(x=65.0, y=(130.0 + 100.0 * i), width=219.0, height=70.0)
+    button_1.place(
+        x=0.045 * width,
+        y=(0.136 * height + 100.0 * i),
+        width=0.15 * width,
+        height=0.073 * height,
+    )
 
     main_frame_buttons.append(button_1)
 
@@ -320,8 +326,8 @@ for i in range(0, 6):
 canvas_inicio = tk.Canvas(
     frame_inicio,
     bg="#FFFFFF",
-    height=950,
-    width=1440,
+    height=height,
+    width=width,
     bd=0,
     highlightthickness=0,
     relief="ridge",
@@ -329,10 +335,10 @@ canvas_inicio = tk.Canvas(
 canvas_inicio.place(x=0, y=0)
 
 start_image_1 = tk.PhotoImage(file=relative_to_assets("image_1.png", "frame2"))
-st_image_1 = canvas_inicio.create_image(720.0, 475.0, image=start_image_1)
+st_image_1 = canvas_inicio.create_image(width / 2.0, height / 2.0, image=start_image_1)
 
 start_image_2 = tk.PhotoImage(file=relative_to_assets("image_2.png", "frame2"))
-st_image_2 = canvas_inicio.create_image(width / 2, height / 2, image=start_image_2)
+st_image_2 = canvas_inicio.create_image(width / 2.0, height / 2.0, image=start_image_2)
 
 start_image_3 = tk.PhotoImage(file=relative_to_assets("image_3.png", "frame2"))
 # st_image_3 = canvas_inicio.create_image(1304.0, 906.0, image=start_image_3)
@@ -367,8 +373,8 @@ class TelaNewTask:
         self.canvas = tk.Canvas(
             parent_frame,
             bg="#FFFFFF",
-            height=950,
-            width=1440,
+            height=height,
+            width=width,
             bd=0,
             highlightthickness=0,
             relief="ridge",
@@ -424,27 +430,31 @@ class TelaNewTask:
         )
 
         # self.image_1_id = self.canvas.create_image(1000.0, 500.0, image=self.image_1)
-        self.image_2_id = self.canvas.create_image(720.0, 0.0, image=self.image_2)
+        self.image_2_id = self.canvas.create_image(width / 2.0, 0.0, image=self.image_2)
 
         texts_info = [
-            (140.0, "Nome da tarefa:"),
-            (210.0, "Data da tarefa (dd/mm/aaaa):"),
-            (280.0, "Horário da tarefa (hh:mm):"),
-            (350.0, "Descrição:"),
-            (420.0, "Status da tarefa:"),
-            (500.0, "Prioridade da tarefa:"),
-            (580.0, "Categoria da tarefa (Ex.: Estudos, Trabalho...):"),
+            (0.169 * height, "Nome da tarefa:"),
+            (0.254 * height, "Data da tarefa (dd/mm/aaaa):"),
+            (0.338 * height, "Horário da tarefa (hh:mm):"),
+            (0.423 * height, "Descrição:"),
+            (0.508 * height, "Status da tarefa:"),
+            (0.604 * height, "Prioridade da tarefa:"),
+            (0.701 * height, "Categoria da tarefa (Ex.: Estudos, Trabalho...):"),
         ]
 
-        for y, text in texts_info:
-            self.canvas.create_text(
-                548.0,
-                y,
-                anchor="nw",
-                text=text,
-                fill="#FFFFFF",
-                font=("Inter SemiBold", 14),
-            )
+        for i, text in enumerate(texts_info):
+            tk.Label(
+                self.canvas, text=text[1], bg="#05ACF5", font=("Calibri Bold", 13)
+            ).place(x=0.27 * width, y=text[0])
+
+            # self.canvas.create_text(
+            #     0.27 * width,
+            #     y,
+            #     anchor="nw",
+            #     text=text,
+            #     fill="#FFFFFF",
+            #     font=("Calibri Bold", 13),
+            # )
 
         # self.entry_bg_1 = self.canvas.create_image(
         #     719.0, 272.5, image=self.entry_image_1
@@ -463,13 +473,36 @@ class TelaNewTask:
         # )
 
         # Criando as caixas de texto
-        entry_y = [170, 240, 310, 380, 610]
+        # (0.169 * height, "Nome da tarefa:"),
+        # (0.254 * height, "Data da tarefa (dd/mm/aaaa):"),
+        # (0.338 * height, "Horário da tarefa (hh:mm):"),
+        # (0.423 * height, "Descrição:"),
+        # (0.508 * height, "Status da tarefa:"),
+        # (0.604 * height, "Prioridade da tarefa:"),
+        # (0.701 * height, "Categoria da tarefa (Ex.: Estudos, Trabalho...):"),
+
+        entry_y = [
+            texts_info[0][0],
+            texts_info[1][0],
+            texts_info[2][0],
+            texts_info[3][0],
+            texts_info[6][0],
+        ]
+        tamanhos = []
+        font = tkfont.Font(family="Calibri", weight="bold", size=13)
+        for i, j in enumerate(texts_info):
+            if i != 4 and i != 5:
+                size = font.measure(j[1])
+                tamanhos.append(size)
+
+        largura_caixas = [300, 100, 100, 300, 200]
         self.entries = []
         for i in range(0, 5):
             entry = tk.Entry(
                 parent_frame, bd=0, bg="#D9D9D9", fg="#000716", highlightthickness=0
             )
-            entry.place(x=548.0, y=entry_y[i], height=24, width=350)
+            x = tamanhos[i] + 10 + 0.27 * width
+            entry.place(x=x, y=entry_y[i] + 3, height=24, width=largura_caixas[i])
             self.entries.append(entry)
 
         self.entry_dicionario = {
@@ -488,7 +521,7 @@ class TelaNewTask:
             command=lambda: self.setStatus(Status.PARA_EXECUTAR),
             relief="flat",
         )
-        self.button_1.place(x=548.0, y=450.0, width=94.0, height=38.0)
+        self.button_1.place(x=0.40 * width, y=0.50 * height, width=94.0, height=38.0)
         self.button_2 = tk.Button(
             parent_frame,
             image=self.button_image_2,
@@ -497,7 +530,7 @@ class TelaNewTask:
             command=lambda: self.setStatus(Status.EXECUTANDO),
             relief="flat",
         )
-        self.button_2.place(x=673.0, y=450.0, width=94.0, height=38.0)
+        self.button_2.place(x=0.52 * width, y=0.50 * height, width=94.0, height=38.0)
         self.button_3 = tk.Button(
             parent_frame,
             image=self.button_image_3,
@@ -506,9 +539,15 @@ class TelaNewTask:
             command=lambda: self.setStatus(Status.CONCLUIDA),
             relief="flat",
         )
-        self.button_3.place(x=798.0, y=450.0, width=94.0, height=38.0)
+        self.button_3.place(x=0.64 * width, y=0.50 * height, width=94.0, height=38.0)
 
-        self.canvas.create_rectangle(920, 450, 958, 488, fill="#333333")
+        self.canvas.create_rectangle(
+            0.76 * width,
+            0.50 * height,
+            0.76 * width + 38,
+            0.50 * height + 38,
+            fill="#333333",
+        )
 
         self.button_4 = tk.Button(
             parent_frame,
@@ -518,7 +557,7 @@ class TelaNewTask:
             command=lambda: self.setPrioridade(Prioridade.BAIXA),
             relief="flat",
         )
-        self.button_4.place(x=548.0, y=530.0, width=94.0, height=38.0)
+        self.button_4.place(x=0.43 * width, y=0.60 * height, width=94.0, height=38.0)
         self.button_5 = tk.Button(
             parent_frame,
             image=self.button_image_5,
@@ -527,7 +566,7 @@ class TelaNewTask:
             command=lambda: self.setPrioridade(Prioridade.MEDIA),
             relief="flat",
         )
-        self.button_5.place(x=672.0, y=530.0, width=94.0, height=38.0)
+        self.button_5.place(x=0.55 * width, y=0.60 * height, width=94.0, height=38.0)
         self.button_6 = tk.Button(
             parent_frame,
             image=self.button_image_6,
@@ -536,9 +575,15 @@ class TelaNewTask:
             command=lambda: self.setPrioridade(Prioridade.ALTA),
             relief="flat",
         )
-        self.button_6.place(x=798.0, y=530.0, width=94.0, height=38.0)
+        self.button_6.place(x=0.67 * width, y=0.60 * height, width=94.0, height=38.0)
 
-        self.canvas.create_rectangle(920, 530, 958, 568, fill="#333333")
+        self.canvas.create_rectangle(
+            0.79 * width,
+            0.60 * height,
+            0.79 * width + 38,
+            0.60 * height + 38,
+            fill="#333333",
+        )
 
         self.button_image_concluir = tk.PhotoImage(
             file=assets("button_7.png", "frame3")
@@ -550,7 +595,7 @@ class TelaNewTask:
             highlightthickness=0,
             relief="flat",
         )
-        self.button_concluir.place(x=548.0, y=650.0)
+        self.button_concluir.place(x=0.273 * width, y=0.78 * height)
 
         self.button_image_8 = tk.PhotoImage(file=assets("button_8.png", "frame3"))
         self.button_8 = tk.Button(
@@ -561,7 +606,7 @@ class TelaNewTask:
             command=lambda: mainScreen(),
             relief="flat",
         )
-        self.button_8.place(x=758.0, y=650.0)
+        self.button_8.place(x=0.55 * width, y=0.78 * height)
 
     def setPrioridade(self, num):
         self.prioridade = num
@@ -572,7 +617,13 @@ class TelaNewTask:
             color = "#FFFF00"
         elif self.prioridade == Prioridade.ALTA:
             color = "#00FF00"
-        self.canvas.create_rectangle(920, 530, 958, 568, fill=color)
+        self.canvas.create_rectangle(
+            0.79 * width,
+            0.60 * height,
+            0.79 * width + 38,
+            0.60 * height + 38,
+            fill=color,
+        )
 
     def setStatus(self, num):
         self.status = num
@@ -583,7 +634,13 @@ class TelaNewTask:
             color = "#FFFF00"
         elif self.status == Status.CONCLUIDA:
             color = "#00FF00"
-        self.canvas.create_rectangle(920, 450, 958, 488, fill=color)
+        self.canvas.create_rectangle(
+            0.76 * width,
+            0.50 * height,
+            0.76 * width + 38,
+            0.50 * height + 38,
+            fill=color,
+        )
 
     def concluir(self, agenda):
         self.name = self.entries[self.entry_dicionario.get("nome")].get()
